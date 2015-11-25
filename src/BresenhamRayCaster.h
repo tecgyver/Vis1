0b42a553f2669ea58c3e72d09a93d0f62e5e0736
Juergen Schefcik <jschefcik@cg.tuwien.ac.at>
2015-11-25 14:16:59 +0100

    added bin and tmp directory

--- src/BresenhamRayCaster.h
+++ src/BresenhamRayCaster.h
@@ -1,12 +1,15 @@
 #pragma once
 
 #include <vector>
+#include <qvector3d.h>
+
+#include "Volume.h"
 
 class BresenhamRayCaster
 {
 public:
 	BresenhamRayCaster();
 	~BresenhamRayCaster();
-	//std::vector<float> cast(float x, float y, float z); //Parameters: start position, direction
+	//std::vector<float> cast(QVector3D& location, QVector3D& direction, Volume& data); //Parameters: start position, direction
 };
 
